ca3234f59ab18b1874db44aff6aa03fd027a2fef
HanochShetty <hanochs57@gmail.com>
2026-09-22 09:19:37 +0530

    Add project files.

---
+++ EcommercePWA(pract10)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommercePWA_pract10_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EcommercePWA(pract10)/wwwroot/app.js
@@ -0,0 +1,21 @@
+﻿if ('ServiceWorker' in navigator) {
+    navigator.addServiceWorker('service-worker.js')
+        .then(() => { console.log('Service Worker Registered') })
+} 
+let cart = JSON.parse(localStorage.getItem('cart')) || []
+displayCart();
+
+function addToCart(product) {
+    cart.push(product);
+    localStorage.setItem('cart', JSON.stringify(cart),
+        displayCart());
+}
+function displayCart() {
+    let cartList = document.getElementById("cartList");
+    cartList.innerHTML = '';
+    cart.forEach(item => {
+        let li = document.createElement('li');
+        li.textContent = item;
+        cartList.appendChild(li);
+    });
+}
